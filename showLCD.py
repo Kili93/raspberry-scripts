07830e8088fdd8597c9e469c33290221cb3d2528
Kili <kilian.bissig@hotmail.com>
2015-05-27 16:07:05 +0200

    Continue Update showLCD

--- showLCD.py
+++ showLCD.py
@@ -13,40 +13,44 @@ ENABLE_LCD = 0
 STATS = 0
 SITE = 4
 CURRENT = ''
-while not(lcd.is_pressed(LCD.RIGHT)):
-        lcd.home()
+STOP = 0
+while STOP == 0:
+        if lcd.is_pressed(LCD.RIGHT):
+                STOP = 1
         CURRENT = commands.getoutput('mpc current')
         if CURRENT is not None:
                 lcd.enable_display(1)
                 lcd.set_backlight(1)
                 LINE1 = showInfo.showCurrentArtist()
                 LINE2 = showInfo.showCurrentTitle()
-        if SITE == 3:
-                LINE1 = 'Volume'
-                LINE2 = showInfo.showVolume()
-        if SITE == 4:
+        if lcd.is_pressed(LCD.SELECT):
+                if CURRENT is not None:
+                        commands.getoutput('mpc stop')
+                else:
+                        commands.getoutput('mpc play')
+        if STATS == 1:
                 LINE1 = showInfo.showIP()
                 LINE2 = showInfo.showUptime()
-        if SITE == 5:
+        if STATS == 2:
                 LINE1 = 'CPU :' +str(showInfo.showCPU())+'%'
                 LINE2 = 'RAM :' +str(showInfo.showRAM())+'%'
-        if SITE == 6:
+        if STATS == 3:
                 LINE1 = 'root: ' +str(showInfo.showDiskUsageRoot())+'%'
                 LINE2 = 'USB : ' +str(showInfo.showDiskUsageUSB())+'%'
-        if SITE == 7:
+        if STATS == 4:
                 LINE1 = 'Datum :' +showInfo.showDate()
                 LINE2 = 'Zeit  :' +showInfo.showTime()
-        if SITE == 13:
+        if STATS == 5:
                 MESSAGE = 'Temperatur      :\n' +showInfo.showTemp()+' C'
         if lcd.is_pressed(LCD.UP):
                 //Volume up
         if lcd.is_pressed(LCD.DOWN):
                 //Volume Down
         if lcd.is_pressed(LCD.LEFT):
-                if STATS == 1:
-                        SITE = SITE + 1
+                if STATS > 5:
+                        STATS = 0
                 else:
-                        STATS = 1      
+                        STATS = STATS +1
        while len(LINE1) < 16:
                 LINE1 = LINE1 + ' '
         while len(LINE2) < 16:
